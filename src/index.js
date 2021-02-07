05a7afca324c34bec53e92275e3889c3f70c9dfb
Kate <nekl47511@gmail.com>
2021-02-07 20:16:12 +0200

    Solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    let min = Math.min.apply(null, array);
+
+    if (isFinite(min)) {
+        return min
+    } else {
+        return 0;
+    }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+
+    let max = Math.max.apply(null, array);
+
+    if (isFinite(max)) {
+        return max
+    } else {
+        return 0;
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+
+
+    let arraySum = 0;
+    if (Array.isArray(array)) {
+        let sum = array.length;
+        if (array.length !== 0) {
+            for (i = 0; i < sum; i++) {
+                arraySum += array[i];
+            }
+            return arraySum / sum;
+        } else {
+            return 0;
+        }
+
+    }
+    if (!Array.isArray(array)) {
+        return 0;
+    }
 }
